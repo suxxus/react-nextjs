4f30c5cc84faa4d27f2c8357babafafab00ffc7d
sxs <sxs@xps.com>
2023-04-28 13:34:43 +0200

    Set default colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home(): JSX.Element {
   return (
-    <main>
+    <main className="w-full">
       <h1>hello next</h1>
+      <ul className="p-10 m-5 border border-purple-900">
+        <li>
+          <a href="/users">users</a>
+        </li>
+      </ul>
     </main>
   );
 }
